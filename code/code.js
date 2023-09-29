9a8739898fc8acd7d0f7f3c56d2feb51918b5ee6
Blackie24x <kacper.kraczkowski24@gmail.com>
2023-09-29 11:13:11 +0200

    feat: create composition section

--- code/code.js
+++ code/code.js
@@ -1,5 +1,4 @@
 window.addEventListener('load', () => {
 	const topbarHeight = document.querySelector('.topbar').offsetHeight
-	console.log(topbarHeight)
 	document.body.style.marginTop = `${topbarHeight}px`
 })
